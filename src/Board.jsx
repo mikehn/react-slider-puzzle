69384ad61c384700dd8cac37702a940f3d943829
mhasin <michael.hasin@intel.com>
2018-11-13 11:41:29 +0200

    Box component addition
     * added box component and styling
     * added styling to board and some cleanup

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-
+import Box from "./Box";
+import "./Board.css";
 class Board extends Component {
 
     constructor(props) {
         super(props);
         const SIZE = props.size || 3;
+        // Note: you should avoid placing view elements in state,
+        //       state elements should only be changed by state functions
         this.state = {
             board: this.initBord(SIZE)
         }
     }
 
+    /**
+     * Gets a new board of the given size
+     * @param {Number} size amount of Boxes per row
+     */
     initBord(size) {
         let board = [];
-        let cellNum=0;
+        let cellNum = 0;
         for (let i = 0; i < size; i++) {
             let row = [];
             for (let j = 0; j < size; j++) {
@@ -23,16 +30,23 @@ class Board extends Component {
         return board;
     }
 
-    getRow(rowData) {
-        return <div>{rowData}</div>
+    /**
+     * returns a single slider row given the row data
+     * @param {Object} rowData row data
+     * @param {Number} i row number
+     */
+    getRow(rowData, i) {
+        return (
+            <div key={i} className="slider-row">
+                {rowData.map(bNum => <Box key={bNum} boxNumber={bNum} />)}
+            </div>
+        );
     }
 
     render() {
-
         let rows = this.state.board.map(this.getRow);
-
         return (
-            <div>
+            <div className="slider-board">
                 {rows}
             </div>
         );
---
+++ src/Box.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./Box.css";
+
+// Stateless react component
+// Note: the {boxNumber} is ES6 feature called Destructuring Assignment
+// read all about it here: http://es6-features.org/#ParameterContextMatching 
+export default function Box({boxNumber}){
+    return <span className="slider-box">{boxNumber}</span>;
+}
